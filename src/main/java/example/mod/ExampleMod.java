f28d149e491d56d61df511295274b35943b97db1
Farn <93525542+FarnGitHub@users.noreply.github.com>
2026-09-09 19:19:05 +0700

    update example mod

--- src/main/java/example/mod/ExampleMod.java
+++ src/main/java/example/mod/ExampleMod.java
@@ -1,11 +1,42 @@
 package example.mod;
 
-import net.fabricmc.api.ClientModInitializer;
+import example.mod.block.ExampleBlock;
+import net.minecraft.src.*;
 
-public class ExampleMod implements ClientModInitializer {
+import java.io.InputStreamReader;
+import java.util.Map;
+import java.util.Properties;
+
+public class ExampleMod extends BaseMod {
+
+    //this will generate config file somewhere
+    @MLProp(name="Example Block's ID")
+    public static int exampleId = 255;
+
+    Block exampleBlock;
+
+    public ExampleMod() {
+        System.out.println("[ExampleMod] Initializing");
+        readCustomLang();
+        exampleBlock = new ExampleBlock(exampleId).setBlockName("smiles");
+        exampleBlock.blockIndexInTexture = ModLoader.addOverride("/gui/items.png", "/assets/example_mod/textures/smile.png");
+    }
 
     @Override
-    public void onInitializeClient() {
-        System.out.println("[ExampleMod] Initializing");
+    public String Version() {
+        return "1.0";
+    }
+
+    //There will be api for reading custom lang file soon, this is placeholder for now
+    public void readCustomLang() {
+        try {
+            Properties prop = new Properties();
+            prop.load(new InputStreamReader(ExampleMod.class.getResourceAsStream("/assets/example_mod/lang/en_US.lang")));
+            for (Map.Entry<Object, Object> entry : prop.entrySet()) {
+                ModLoader.AddLocalization((String)entry.getKey(), (String)entry.getValue());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
---
+++ src/main/java/example/mod/block/ExampleBlock.java
@@ -0,0 +1,10 @@
+package example.mod.block;
+
+import net.minecraft.src.Block;
+import net.minecraft.src.Material;
+
+public class ExampleBlock extends Block {
+    public ExampleBlock(int i1) {
+        super(i1, Material.iron);
+    }
+}
